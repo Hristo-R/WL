123d3e85444f6525760d84d981ca35e10211c2cb
Hristo-R <hristo.razpopov@abv.bg>
2019-01-18 01:20:25 +0200

    Edited Startup.cs -> app.UseMvc(routes => ...

--- Holidays/Startup.cs
+++ Holidays/Startup.cs
@@ -64,7 +64,16 @@ namespace Holidays
 
             app.UseAuthentication();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
