4dc2050670322cc6abeecad5d771177399333533
Hristo-R <hristo.razpopov@abv.bg>
2019-01-17 21:32:36 +0200

    Edited Holidays/Common/ServiceCollectionExtensions.cs and moved _ViewStart.cshtml

--- Holidays/Common/ServiceCollectionExtensions.cs
+++ Holidays/Common/ServiceCollectionExtensions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Holidays.Web.Common
+﻿namespace Holidays.Web.Common
 {
     public class ServiceCollectionExtensions
     {
