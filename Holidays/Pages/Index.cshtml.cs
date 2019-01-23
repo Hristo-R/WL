14e2606e5a28d3ad857d76f357bc6ec4821720ff
Hristo-R <hristo.razpopov@abv.bg>
2019-01-23 11:46:20 +0200

    Razor pages - Authorization

--- Holidays/Pages/Index.cshtml.cs
+++ Holidays/Pages/Index.cshtml.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace Holidays.Pages
+﻿namespace Holidays.Pages
 {
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+   // [Authorize]
     public class IndexModel : PageModel
     {
         public void OnGet()
