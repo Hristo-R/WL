e7d72563563749459b2eac8911330de11f9f2eb2
Hristo-R <hristo.razpopov@abv.bg>
2019-01-28 02:20:07 +0200

    Added HotelOlympicBibis.cs

---
+++ Holidays.Models/HotelOlympicBibis.cs
@@ -0,0 +1,19 @@
+﻿namespace Holidays.Models
+{
+    public class HotelOlympicBibis
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Galery { get; set; }
+
+        public string Booking { get; set; }
+
+        public HotelOlympicBibisTable Table { get; set; }
+
+        public string AdditionalDescription { get; set; }
+    }
+}
