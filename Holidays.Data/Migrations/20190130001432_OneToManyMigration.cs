499a950c16e99be71a0be4f81b2b3ae6f89803bf
Hristo-R <hristo.razpopov@abv.bg>
2019-01-30 02:26:40 +0200

    PM> Add-Migration InitialMigration PM> Update-Database

---
+++ Holidays.Data/Migrations/20190130001432_OneToManyMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Holidays.Data.Migrations
+{
+    public partial class OneToManyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HotelOlympicBibisTable_HotelOlympicBibisId",
+                table: "HotelOlympicBibisTable");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelOlympicBibisTable_HotelOlympicBibisId",
+                table: "HotelOlympicBibisTable",
+                column: "HotelOlympicBibisId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_HotelOlympicBibisTable_HotelOlympicBibisId",
+                table: "HotelOlympicBibisTable");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelOlympicBibisTable_HotelOlympicBibisId",
+                table: "HotelOlympicBibisTable",
+                column: "HotelOlympicBibisId",
+                unique: true);
+        }
+    }
+}
