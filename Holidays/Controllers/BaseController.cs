7f3bb9b7b8127948c1a563bbc575d32278ecadad
Hristo-R <hristo.razpopov@abv.bg>
2019-02-01 21:08:39 +0200

    added BaseController.cs

---
+++ Holidays/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿namespace Holidays.Web.Controllers
+{
+    using Holidays.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    public abstract class BaseController : Controller
+    {
+        private readonly HolidaysDbContext db;
+
+        protected BaseController()
+        {
+            this.Db = db;
+        }
+
+        public HolidaysDbContext Db { get; }
+    }
+}
