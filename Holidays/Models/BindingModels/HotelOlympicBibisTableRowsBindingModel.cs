fbe65a9144183804643262096c3912e893a6f78b
Hristo-R <hristo.razpopov@abv.bg>
2019-02-26 15:38:18 +0200

    Added HotelOlympicBibisTableRowsBindingModel.cs

---
+++ Holidays/Models/BindingModels/HotelOlympicBibisTableRowsBindingModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Holidays.Web.Models.BindingModels
+{
+    public class HotelOlympicBibisTableRowsBindingModel
+    {
+        public string Accommodation { get; set; }
+
+        public string Period01 { get; set; }
+
+        public string Period02 { get; set; }
+
+        public string Period03 { get; set; }
+
+        public string Period04 { get; set; }
+
+        public string Period05 { get; set; }
+
+        public string Period06 { get; set; }
+
+        public string Period07 { get; set; }
+
+        public string Period08 { get; set; }
+
+        public string Period09 { get; set; }
+
+        public string Period10 { get; set; }
+    }
+}
